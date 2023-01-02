47dec5c84ae23a1d22ce016c7e826c36e2613593
Laynaria <pro.laynaria@outlook.fr>
2023-01-02 12:28:21 +0100

    finished the quest 9.1 and 9.2 with both front and back working

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,13 +1,46 @@
 const express = require("express");
 
+// Ajout de multer
+const multer = require("multer");
+
+// Ajouter fs
+const fs = require("fs");
+
+// Ajouter uuid
+const { v4: uuidv4 } = require("uuid");
+
+// Définir la destination des fichiers upload
+const upload = multer({ dest: "uploads/" });
+
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+// Les routes de base du template
+// const itemControllers = require("./controllers/itemControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+// router.get("/items", itemControllers.browse);
+// router.get("/items/:id", itemControllers.read);
+// router.put("/items/:id", itemControllers.edit);
+// router.post("/items", itemControllers.add);
+// router.delete("/items/:id", itemControllers.destroy);
+// Fin des routes de base du template
+
+// Nos routes!
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  // on récupère le nom original
+  const { originalname } = req.file;
+
+  // on récupère le nom du fichier
+  const { filename } = req.file;
+
+  // on utilise la fonction fs rename
+  fs.rename(
+    `uploads/${filename}`,
+    `uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 module.exports = router;
