e21c8496509f3e9703623bf94e44d98c900bcb92
Laynaria <pro.laynaria@outlook.fr>
2023-01-02 12:26:58 +0100

    finished the quest 9.1 and 9.2 with both front and back working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
+import axios from "axios";
+import { useRef } from "react";
 import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+
+  function hSubmit(evt) {
+    evt.preventDefault();
+
+    const formData = new FormData();
+    formData.append("avatar", inputRef.current.files[0]);
+
+    axios.post("http://localhost:5000/api/avatar", formData);
+  }
+
   return (
     <div className="App">
+      <form encType="multipart/form-data" onSubmit={hSubmit}>
+        <input type="file" name="avatar" ref={inputRef} />
+        <button type="submit">Envoyer</button>
+      </form>
       <Home />
       <p>coucou</p>
     </div>
